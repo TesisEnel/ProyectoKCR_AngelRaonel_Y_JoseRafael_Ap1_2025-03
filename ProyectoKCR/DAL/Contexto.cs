5c0ab1de36a0527278b37cce36be0947dd4b5a06
Jose <Joser2711m@gmail.com>
2025-11-21 12:40:35 -0400

    Creacion de carpeta DAL y contexto

---
+++ ProyectoKCR/DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoKCR.Models;
+
+namespace ProyectoKCR.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    public DbSet<Empleados> empleados { get; set; }
+    public DbSet<Clientes> clientes { get; set; }
+    public DbSet<Materiales> materiales { get; set; }
+    public DbSet<Servicios> servicios { get; set; }
+    public DbSet<PreFacturas> preFacturas { get; set; }
+    public DbSet<Turnos> turnos { get; set; }
+    public DbSet<DetallePreFactura> detallePreFactura { get; set; }
+
+}
