6e51ae9e57213afc76c8e66718f6013f85953f29
angel <angel_guerrero1@ucne.edu.do>
2025-12-11 19:40:53 -0400

    PANTALLA PARA LA CREACION REGISTRO SERVICIO

--- KCR/Models/Materiales.cs
+++ KCR/Models/Materiales.cs
@@ -7,14 +7,18 @@ public class Materiales
 {
     [Key]
     public int IdMaterial { get; set; }
+
     [Required(ErrorMessage = "El nombre es obligatorio.")]
     public string Nombre { get; set; }
-    [Required(ErrorMessage = "La existencia es requerido")]
-    [Range(0, int.MaxValue, ErrorMessage = "La existencia deb ser mayor o igual a 0")]
-    public int Existencia { get; set; } = 0;
-    [Required(ErrorMessage = "El nombre del cliente es requerido")]
-    [Range(0.01, double.MaxValue, ErrorMessage = "El total debe ser mayor que 0")]
+
+    [Required(ErrorMessage = "La existencia es requerida")]
+    [Range(0.0, double.MaxValue, ErrorMessage = "La existencia debe ser mayor o igual a 0")]
+    public double Existencia { get; set; } = 0.0;
+
+    [Required(ErrorMessage = "El precio unitario es requerido")]
+    [Range(0.01, double.MaxValue, ErrorMessage = "El precio unitario debe ser mayor que 0")]
     public double PrecioUnitario { get; set; }
+
     public bool Activo { get; set; } = true;
 
     public ICollection<PreFacturaDetalles> PreFacturaDetalles { get; set; } = new List<PreFacturaDetalles>();
