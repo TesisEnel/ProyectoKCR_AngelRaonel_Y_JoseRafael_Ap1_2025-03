62fb5e0e878eab31a19536e00cb9a2581bf55519
Jose-R-OG <jose_ortega2@ucne.edu.do>
2025-12-15 19:11:52 -0400

    Proyecto Terminado

--- KCR/Program.cs
+++ KCR/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Proyecto Terminado.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
